fdc523724bf1e37cae9466c10c574feb7f4dcb54
Jovin Jijo <jovin96@gmail.com>
2021-09-02 00:01:56 +0530

    feat: save order to backend, show spinner on save

--- ui/src/features/cards-deck/CardsDeck.tsx
+++ ui/src/features/cards-deck/CardsDeck.tsx
@@ -1,21 +1,51 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, CircularProgress } from "@material-ui/core";
 import update from "immutability-helper";
 import Card, { CardDetail } from "../card/Card";
-import { apiCall } from "../../util/Util";
+import { apiCall, useInterval } from "../../util/Util";
 
 export default function CardsDeck() {
+  // cards is used to store the list of cards. When cards are rearranged, the order of cards change
   const [cards, setCards] = useState<CardDetail[]>([]);
+  // saving is used to indicate whether the application is saving the data to backend or not
+  const [saving, setSaving] = useState(false);
+  // lastSaved stores the last saved date
+  const [lastSaved, setLastSaved] = useState(new Date().toLocaleString());
+  // cardsDirty is used to indicate whether the cards order was changed and needs saving
+  const [cardsDirty, setCardsDirty] = useState(false);
 
+  // Call the backend to get all cards initially
   useEffect(() => {
     const fn = async () => {
       try {
         const cards = await apiCall("/api/cards", "GET");
+        // Sort the cards according to position before setting the state
+        cards.sort((a: CardDetail, b: CardDetail) => a.position - b.position);
         setCards(cards);
       } catch (ex) {}
     };
     fn();
   }, []);
+
+  // Call the backend every 5 seconds to update the positions, if it was updated
+  useInterval(async () => {
+    if (cardsDirty) {
+      try {
+        setSaving(true);
+        await apiCall(
+          "/api/cards",
+          "PATCH",
+          // Set the position attribute to the index of the card
+          cards.map((card, idx) => ({ ...card, position: idx }))
+        );
+        setCardsDirty(false);
+        setLastSaved(new Date().toLocaleString());
+      } finally {
+        // Set saving to false after a second so that the Spinner shows for 1 second instead of ~100 ms
+        setTimeout(() => setSaving(false), 1000);
+      }
+    }
+  }, 5000);
 
   const moveCard = useCallback(
     (dragIndex: number, hoverIndex: number) => {
@@ -28,15 +58,34 @@ export default function CardsDeck() {
           ],
         })
       );
+      // Whenever the order is changed, set cardsDirty to true so that it saves it
+      setCardsDirty(true);
     },
     [cards]
   );
 
   return (
     <div>
-      <Typography variant="subtitle1" gutterBottom>
-        Things you can do
-      </Typography>
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        style={{ height: "3rem" }}
+      >
+        <Grid item>
+          <Typography variant="subtitle1" gutterBottom>
+            Things you can do
+          </Typography>
+        </Grid>
+        <Grid item>
+          {saving ? (
+            <CircularProgress color="secondary" size="1.5rem" />
+          ) : (
+            <Typography>Last Saved : {lastSaved}</Typography>
+          )}
+        </Grid>
+      </Grid>
+
       <Grid container spacing={1}>
         {cards.map((item, idx) => (
           <Grid item xs={4} key={item.position}>
--- ui/src/util/Util.ts
+++ ui/src/util/Util.ts
@@ -1,13 +1,39 @@
+import { useEffect, useRef } from "react";
+
 export function getImageUrl(seed: string, width: number, height: number) {
   return `https://picsum.photos/seed/${seed}/${width}/${height}`;
 }
 
-export type HTTPMethod = 'GET' | 'POST';
+export type HTTPMethod = "GET" | "POST" | "PATCH";
 
-export async function apiCall(path: string, method: HTTPMethod, body?: object): Promise<any> {
-    return fetch(path, {
-      method: method,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(body),
-    }).then((response) => response.json());
-  }
+export async function apiCall(
+  path: string,
+  method: HTTPMethod,
+  body?: object
+): Promise<any> {
+  return fetch(path, {
+    method: method,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  }).then((response) => response.json());
+}
+
+// Custom React hook similar to setInterval
+// https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+export function useInterval(callback: Function, delay: number) {
+  const savedCallback = useRef<Function>();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current && savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
